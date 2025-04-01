98f606c3b268dbd5de5268cf5a87976560ad7512
Adrian Sanchez <63045656+Lisztos@users.noreply.github.com>
2025-04-01 21:10:36 +0800

    background

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,8 @@ export default function RootLayout({
   return (
     <html lang="en" className="!scroll-smooth">
       <body className={`${inter.className} bg-gray-50 text-gray-950 relative pt-4 min-h-screen dark:bg-gray-900 dark:text-gray-50 dark:text-opacity-90`}>
-        <div className="bg-[#fbe2e3] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#946263]"></div>
-        <div className="bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[-35rem] h-[31.25rem] w-[50rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] dark:bg-[#676394]"></div>
+        <div className="bg-[#0ea5e9] fixed inset-0 -z-10 w-full max-w-[75rem] mx-auto rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#0369a1]"></div>
+        <div className="bg-[#c4b5fd] fixed inset-0 -z-10 w-full max-w-[50rem] mx-auto rounded-full blur-[10rem] sm:w-[68.75rem] dark:bg-[#5650a7]"></div>
 
         <ThemeContextProvider>
           <ActiveSectionContextProvider>
