e85bd98466e66943c4f290dc53a8538b1ccef412
Adrian Sanchez <isaias.liszt@gmail.com>
2025-05-15 15:51:36 +0200

    Add real CV

--- src/app/sections/Intro.tsx
+++ src/app/sections/Intro.tsx
@@ -117,7 +117,7 @@ export default function Intro() {
         {/* Download CV Button */}
         <a
           className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-black active:scale-105 transition cursor-pointer border border-transparent justify-center w-auto"
-          href="/CV.pdf"
+          href="/adrian-sanchez-cv.pdf"
           download
         >
           Download CV{" "}
